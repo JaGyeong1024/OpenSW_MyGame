30f17089b783e3cd31d0f61301805fc9765e426a
JaGyeong1024 <92356313+JaGyeong1024@users.noreply.github.com>
2021-11-16 15:30:35 +0900

    Update sudoku-game.py

--- sudoku-game.py
+++ sudoku-game.py
@@ -1,4 +1,3 @@
-from sudokutools import valid, solve, find_empty
 from copy import deepcopy
 from sys import exit
 import pygame
@@ -6,6 +5,65 @@ import time
 import random
 pygame.init()
 
+def print_board(board):
+
+    boardString = ''
+    for i in range(9):
+        for j in range(9):
+            boardString += str(board[i][j]) + ' '
+            if (j + 1) % 3 == 0 and j != 0 and j + 1 != 9:
+                boardString += '| '
+
+            if j == 8:
+                boardString += '\n'
+
+            if j == 8 and (i + 1) % 3 == 0 and i + 1 != 9:
+                boardString += '- - - - - - - - - - - \n'
+    print(boardString)
+
+
+def find_empty(board):
+
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+
+
+def valid(board, pos, num):
+
+    for i in range(9):
+        if board[i][pos[1]] == num and (i, pos[1]) != pos:
+            return False
+
+    for j in range(9):
+        if board[pos[0]][j] == num and (pos[0], j) != pos:
+            return False
+
+    start_i = pos[0] - pos[0] % 3
+    start_j = pos[1] - pos[1] % 3
+    for i in range(3):
+        for j in range(3):
+            if board[start_i + i][start_j + j] == num and (start_i + i,
+                    start_j + j) != pos:
+                return False
+    return True
+
+
+def solve(board):
+
+    empty = find_empty(board)
+    if not empty:
+        return True
+
+    for nums in range(9):
+        if valid(board, empty, nums + 1):
+            board[empty[0]][empty[1]] = nums + 1
+
+            if solve(board):
+                return True
+            board[empty[0]][empty[1]] = 0
+    return False
 
 def generate():
 
@@ -150,9 +208,7 @@ class Board:
 
                 return False
 
-
 class Tile:
-
 
     def __init__(
         self,
@@ -308,7 +364,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
 
 main()
 pygame.quit()
