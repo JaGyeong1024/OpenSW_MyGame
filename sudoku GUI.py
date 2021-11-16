d2f7f8adaca1e7f52f8f6c9cab9ce0b4d429db82
JaGyeong1024 <92356313+JaGyeong1024@users.noreply.github.com>
2021-11-16 09:38:20 +0900

    Update sudoku GUI.py

--- sudoku GUI.py
+++ sudoku GUI.py
@@ -195,7 +195,7 @@ def main():
     screen = pygame.display.set_mode((540, 590))
     screen.fill((255, 255, 255))
     pygame.display.set_caption('Sudoku Solver')
-    icon = pygame.image.load('assets/thumbnail.png')
+    icon = pygame.image.load('picture/thumbnail.png')
     pygame.display.set_icon(icon)
 
     font = pygame.font.SysFont('Bahnschrift', 40)
